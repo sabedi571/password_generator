728af8bf92b6f9dfb107d913b32d4e9b183bb783
Syed Abedi <hasnain436@gmail.com>
2020-06-26 18:34:27 -0400

    making progress

--- script.js
+++ script.js
@@ -1,42 +1,87 @@
-var message = "Welcome to the password generator";
-alert(message);
+let caseArray=[];
+let lowerCaseChar=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let upperCaseChar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"];
+let numericChar=["0","1","2","3","4","5","6","7","8","9"];
+let specialChar=["!","@","#","$","%","^","&","*","(",")"];
 
-var message2 = "A good password can protect you from fraud and theft of valuable items such as personal photos and financial documents among other things.";
-alert(message2);
+function generatePassword() {
 
-var message3 = "Please use slider to choose length from 8 to 128 characters and press generate button to execute.";
-alert(message3);
+    let passwordString="";
 
-
-
-
-function generate(){
-    let complexity = document.getElementById("slider").value;
     
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
+    let useLowerCase=confirm("Do you want to include lowercase characters?");
+    let useUpperCase=confirm("Do you want to include uppercase characters?");
+    let useNumeric=confirm("Do you want to include numeric characters?");
+    let useSpecial=confirm("Do you want to include special characters?");
 
-    let password = "";
-
-    for (var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-    }
-
-    document.getElementById("display").value = password;
-
-    document.getElementById("lastNums").innerHTML += password + "<br />";
     
-} 
-     
-
-document.getElementById("length").innerHTML = "Length: 8";
-
-document.getElementById("slider").oninput = function(){
-
-    if(document.getElementById("slider").value > 0){
-        document.getElementById("length").innerHTML = "length: " + document.getElementById("slider").value;
+    while (useLowerCase == false && useUpperCase == false && useNumeric == false && useSpecial == false) {
+        alert("Please select one type of character");
+        useLowerCase=confirm("Do you want to include lowercase characters?");
+        useUpperCase=confirm("Do you want to include uppercase characters?");
+        useNumeric=confirm("Do you want to include numeric characters?");
+        useSpecial=confirm("Do you want to include special characters?");
     }
-    else{
-       document.getElementById("length").innerHTML =  "Lenght: 1";
 
+    
+
+    let passwordLength=parseInt(prompt("How many characters do you want the password to be? The password cannot be less than 8 or more than 128 characters."));
+    
+    while(passwordLength < 8 || passwordLength > 128 || typeof(passwordLength) != "number" || passwordLength === NaN || passwordLength === null) {
+        alert("Please choose a number that is more than 8 and less than 128 characters");
+        passwordLength=parseInt(prompt("How many characters do you want the password to be? The password cannot be less than 8 or more than 128 characters."));
+    } 
+
+    if (useLowerCase==true){
+        caseArray.push(lowerCaseChar);
+    
     }
+    if (useUpperCase==true){
+        caseArray.push(upperCaseChar)
+    
+    }
+    if (useNumeric==true){
+        caseArray.push(numericChar)
+        
+    }
+    if (useSpecial==true){
+        caseArray.push(specialChar)
+    }
+
+   
+
+    for(let i=0;i<passwordLength;i++){
+
+        let randomCharArrayNum;
+        let selectedCharArray;
+        let randomCharNum;
+        let randomChar;
+
+        
+        
+        randomCharArrayNum= parseInt(Math.floor(Math.random()*caseArray.length)); 
+        
+        
+
+        selectedCharArray=caseArray[randomCharArrayNum]; 
+        
+        
+
+        randomCharNum=Math.floor(Math.random()*selectedCharArray.length);
+
+        
+
+        randomChar=selectedCharArray[randomCharNum];
+        
+        passwordString+=randomChar;
+       
+        
+    }
+   
+    passwordEntry.textContent=passwordString;    
 }
+
+
+let generateButton=document.getElementById("generate");
+let passwordEntry=document.getElementById("password");
+generateButton.onclick = generatePassword;
--- script2.js
+++ script2.js
@@ -1,15 +1,18 @@
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+
 var number = ["0","2","3","4","5","6","7","8","9"];
 var specialChar = ["!","@","#","$","%","^","&","*","(",")"];
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-var uppercase = [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+var alphaUpper = [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
 
-var confirmlength = "";
+var confirmLength = "";
 var confirmSpecialCharacter;
-var confirmnumber;
+var confirmNumericCharacter;
 var confirmUpperCase;
 var confirmLowerCase;
 
-function generate() {
+function generatePassword() {
     var confirmlength = (prompt("Enter the amount of characters you would like in your password"));
 
     while(confirmlength <= 8 || confirmlength >= 128) {
@@ -19,18 +22,18 @@ function generate() {
 
     var confirmLowerCase = confirm("click ok to confirm if you would like lower case letters in your password");
     var confirmUpperCase = confirm("click ok to confirm if you would like upper case letters in your password");
-    var confirmnumber = confirm("click ok to confirm if you would like numbers in your password");
+    var confirmNumericCharacter = confirm("click ok to confirm if you would like numbers in your password");
     var confirmSpecialCharacter = confirm("click ok to confirm if you would like special characters in your password");
 
     while(confirmLowerCase === false && confirmUpperCase === false && confirmnumber === false && confirmSpecialCharacter === false) {
         alert("you must choose at least one character type");
         var confirmLowerCase = confirm("click ok to confirm if you would like lower case letters in your password");
         var confirmUpperCase = confirm("click ok to confirm if you would like upper case letters in your password");
-        var confirmnumber = confirm("click ok to confirm if you would like numbers in your password");
+        var confirmNumericCharacter = confirm("click ok to confirm if you would like numbers in your password");
         var confirmSpecialCharacter = confirm("click ok to confirm if you would like special characters in your password");
     }
 
-    var passwordCharacters = []
+      var passwordCharacters = []
 
     if (confirmLowerCase) {
         passwordCharacters = passwordCharacters.concat(lowercase)
@@ -40,7 +43,7 @@ function generate() {
         passwordCharacters = passwordCharacters.concat(uppercase)
     }
 
-    if (confirmnumber) {
+    if (confirmNumericCharacter) {
         passwordCharacters = passwordCharacters.concat(number)
     }
 
@@ -48,16 +51,21 @@ function generate() {
         passwordCharacters = passwordCharacters.concat(specialChar)
     }
 
-    var passwordPicked = ""
+    var randomPassword = ""
 
-    for (var i=0; i < confirmlength; i++) {
-        passwordPicked = passwordPicked + passwordCharacters[Math.floor(Math.random() * passwordCharacters,length)];
+    for (var i = 0; i < confirmlength; i++) {
+        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
         
 
     }
-    return passwordPicked;
+    return randomPassword;
 
 
 }
 
-document.getElementById("display").value = passwordPicked;
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+}
