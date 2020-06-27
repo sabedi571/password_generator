f1a9968e834dbe4cebfbbd4f6a9d13b4bc4bf257
Syed Abedi <hasnain436@gmail.com>
2020-06-26 21:11:45 -0400

    password reset

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-var newCharacterlist=[];
+
 var lowercasealphabets=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var Uppercasealphabets=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"];
 var numbers=["0","1","2","3","4","5","6","7","8","9"];
 var special=["!","@","#","$","%","^","&","*","(",")"];
 
 function generaterandomPassword() {
+
+    var newCharacterlist=[];
 
     
 
